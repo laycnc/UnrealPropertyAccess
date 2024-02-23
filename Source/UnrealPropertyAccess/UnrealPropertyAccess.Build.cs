40839e8ab077bb1bad4b899cc6af3be4cd92b153
laycnc <getsingleton@gmail.com>
2024-02-23 16:45:30 +0900

    cpp17でビルドエラーになる不具合を修正

--- Source/UnrealPropertyAccess/UnrealPropertyAccess.Build.cs
+++ Source/UnrealPropertyAccess/UnrealPropertyAccess.Build.cs
@@ -7,6 +7,8 @@ public class UnrealPropertyAccess : ModuleRules
 	public UnrealPropertyAccess(ReadOnlyTargetRules Target) : base(Target)
 	{
 
+		CppStandard = CppStandardVersion.Cpp17;
+
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
